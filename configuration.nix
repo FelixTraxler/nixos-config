52ab9cc08cf03db155b329d359a96417c057769e
Felix Traxler <contact@felix-traxler.at>
2026-03-11 19:55:41 +0100

    setup nvf

--- configuration.nix
+++ configuration.nix
@@ -106,6 +106,20 @@
   # Install firefox.
   programs.firefox.enable = true;
 
+  programs.nvf = {
+    enable = true;
+
+    # Your settings need to go into the settings attribute set
+    # most settings are documented in the appendix
+    settings = {
+      vim.viAlias = false;
+      vim.vimAlias = true;
+      vim.lsp = {
+        enable = true;
+      };
+    };
+  };
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
--- flake.nix
+++ flake.nix
@@ -4,11 +4,15 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nvf = {
+      url = "github:NotAShelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nixos-hardware }: {
+  outputs = { self, nixpkgs, nixos-hardware, nvf }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      modules = [ ./configuration.nix nixos-hardware.nixosModules.framework-amd-ai-300-series ];
+      modules = [ ./configuration.nix nixos-hardware.nixosModules.framework-amd-ai-300-series nvf.nixosModules.default ];
     };
   };
 }

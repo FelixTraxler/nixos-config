2112ca243468d9d0971f6ee6619d18eb17d5a832
Felix Traxler <contact@felix-traxler.at>
2026-03-11 19:55:41 +0100

    move some pkgs to system level

--- configuration.nix
+++ configuration.nix
@@ -85,13 +85,7 @@
     description = "felix";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-    #  thunderbird
-	vim
 	bitwarden-desktop
-	git
-	lazygit
-	tmux
-	ghostty
     ];
   };
 
@@ -101,11 +95,12 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    vim
+    git
+    lazygit
+    tmux
+    ghostty
   ];
 
   # Some programs need SUID wrappers, can be configured further or are

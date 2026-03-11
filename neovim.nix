e3fe088a782ad502f0ee6058e15e0b95c2dbd907
Felix Traxler <contact@felix-traxler.at>
2026-03-11 19:55:41 +0100

    add nix lsp and autoformat

--- neovim.nix
+++ neovim.nix
@@ -4,11 +4,23 @@
   programs.nvf = {
     enable = true;
 
-    settings = {
-      vim.viAlias = false;
-      vim.vimAlias = true;
-      vim.lsp = {
+    settings.vim = {
+      viAlias = false;
+      vimAlias = true;
+      lsp = {
         enable = true;
+
+        formatOnSave = true;
+      };
+
+      languages = {
+        enableFormat = true;
+        enableTreesitter = true;
+        enableExtraDiagnostics = true;
+
+        # Languages that will be supported in default and maximal configurations.
+        nix.enable = true;
+        markdown.enable = true;
       };
     };
   };

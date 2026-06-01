9ecc9bd83f7cdf45e52501a86645cb466c57fe34
Felix Traxler <contact@felix-traxler.at>
2026-06-01 23:39:12 +0100

    hypr: switch to sane cursor

--- home.nix
+++ home.nix
@@ -185,6 +185,14 @@ in {
     };
   };
 
+  home.pointerCursor = {
+    name = "Adwaita";
+    package = pkgs.adwaita-icon-theme;
+    size = 24;
+    gtk.enable = true;
+    x11.enable = true;
+  };
+
   xdg.desktopEntries.firefox-video = {
     name = "Firefox";
     exec = "firefox %f";
